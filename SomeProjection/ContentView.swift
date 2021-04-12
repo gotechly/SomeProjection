65ff1f93b8b8b5be45920c04084c18f16b8ee859
Joshua Johnson <joshua.elia.johnson@gmail.com>
2021-04-11 23:33:48 -0400

    Initial Commit

---
+++ SomeProjection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SomeProjection
+//
+//  Created by Joshua Johnson on 4/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SomeProjection/SomeProjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeProjectionApp.swift
+//  SomeProjection
+//
+//  Created by Joshua Johnson on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SomeProjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
